4e0ac5a4d54fee3e45663ba619fded2782c8e153
gyeongisung <gisung.gyeon@gmail.com>
2023-09-19 11:02:23 +0900

    feat: Marker 추가

--- components/common/home/MapScene.tsx
+++ components/common/home/MapScene.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import Map from './Map';
+import { NaverMap } from '@/types/map';
+import useMap from '@/hooks/useMap';
+import Markers from './Markers';
+
+// Marker를 그려야 한다.
+// Marker는 naver.map 객체에 그려야 한다.(전역 참조가 가능해야 한다.)
 
 const MapScene = () => {
-  const onLoad = () => {
+  const { initializeMap } = useMap();
+  const onLoadMap = (map: NaverMap) => {
     console.log('로드 완료~');
+    initializeMap(map);
   };
   return (
     <>
-      <Map onLoad={onLoad} />
+      <Map onLoad={onLoadMap} />
+      <Markers />
     </>
   );
 };
--- hooks/useMap.ts
+++ hooks/useMap.ts
@@ -1,9 +1,23 @@
+// SWR을 이용해서 NAVER.MAP을 전역으로 참조 가능하도록
+// MAP 객체를 보관
+import { mutate } from 'swr';
+import { NaverMap } from '@/types/map';
+import { useCallback } from 'react';
 import { Cordinates } from '@/types/info';
 
 export const INITIAL_CENTER: Cordinates = [37.3595704, 127.105399];
 export const INITIAL_ZOOM = 10;
 export const INITIAL_MIN = 6;
-
-const useMap = () => {};
+// 네이버 앱 전역 저장하기
+export const MAP_KEY = '/map';
+const useMap = () => {
+  const initializeMap = useCallback((map: NaverMap) => {
+    // 지도를 보관한다.
+    mutate(MAP_KEY, map);
+  }, []);
+  return {
+    initializeMap,
+  };
+};
 
 export default useMap;
