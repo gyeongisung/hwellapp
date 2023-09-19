16901c85086b467e2754e440ddda0e8a3ae0f154
gyeongisung <gisung.gyeon@gmail.com>
2023-09-19 11:44:00 +0900

    feat: SEO 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import MapScene from '@/components/common/home/MapScene';
 import useInfo from '@/hooks/useInfo';
 import { Info } from '@/types/info';
 import { useEffect } from 'react';
+// SEO 적용하기
+import { NextSeo } from 'next-seo';
 
 interface IProps {
   infos: Info[];
@@ -21,6 +23,10 @@ export default function Home({ infos }: IProps) {
   }, [initializeInfos, infos]);
   return (
     <>
+      <NextSeo
+        title="건강검진센터 위치"
+        description="건강검진센터 위치 서비스 입니다."
+      />
       <Header />
       <MapScene />
     </>
